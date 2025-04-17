989dc367beb4e7f3eaf5217882fed9b9f2989eaf
bourraOmar <bwrtmr@gmail.com>
2025-04-17 10:53:01 +0100

    create route films

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\TestController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 
 Route::get('/', [AuthController::class, 'index']);
 
